76f787c7f01e7116f13edf43c283f1827cd0b68c
Nat Hamilton <nat@tacticmarketing.com>
2017-06-12 17:37:22 -0500

    got mocha and chai testing set up, created an example test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai')
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () =>
+      expect([1, 2, 3].indexOf(4)).to.eql(-1))
+  })
+})
